1a9566f071b0e6e7e2889fd9d306dfccbfa903dc
Álvaro Stein <alvarostein@hotmail.com.br>
2025-06-24 11:10:45 -0300

    add raycast at cursor function

--- scenes/main scenes/main.gd
+++ scenes/main scenes/main.gd
@@ -21,3 +21,16 @@ func _on_change_scene_to(next_scene: String):
 
 func connect_change_scene_signals(scene: Node):
 	scene.connect("change_scene_to", _on_change_scene_to)
+
+
+# Returns an Array of Dictionaries, each containing a colliding object. Empty if no collision.
+func raycast_at_cursor(collision_mask: int) -> Array[Dictionary]:
+	# Get the 2D physics space state from the current world.
+	var space_state = get_world_2d().direct_space_state
+	var mouse_pos = get_global_mouse_position()
+	# Create the query parameters object.
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = mouse_pos
+	query.collision_mask = collision_mask
+	# Returns an Array[Dictionary]
+	return space_state.intersect_point(query)
