26955417e941cf258df55fe5881f5d79cfa59440
Álvaro Stein <alvarostein@hotmail.com.br>
2025-06-25 20:18:03 -0300

    menu finalizado

--- scenes/Menu/Configuracoes/menu_configuracao.gd
+++ scenes/Menu/Configuracoes/menu_configuracao.gd
@@ -1,14 +1,19 @@
 extends Control
 
-signal change_scene_to
+
+func _input(event):
+	if event.is_action_pressed("Esc"):
+		self.get_parent().close_option()
+
+
+func _on_sair_button_pressed() -> void:
+	self.get_parent().close_option()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_parent().connect_change_scene_signals(self)
-	
-	for _button in get_tree().get_nodes_in_group("button_config"):
-		_button.pressed.connect(_on_button_pressed.bind(_button))
+	#for _button in get_tree().get_nodes_in_group("button_config"):
+		#_button.pressed.connect(_on_button_pressed.bind(_button))
 	
 	$MarginContainer/VBoxContainer/ControleVolume.value = (AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Menu")))
 	$MarginContainer/VBoxContainer/ControleVolume2.value = (AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Botoes menu")))
@@ -30,19 +35,14 @@ func _ready() -> void:
 		#AudioServer.set_bus_mute(AudioServer.get_bus_index("somjogo"), false)
 
 
-func _input(event):
-	if event.is_action_pressed("Esc"):
-		emit_signal("change_scene_to", "Menu")
-
-
-func _on_button_pressed(_button : Button) -> void:
-	var sound_player = [$botaosom, $botaosom2, $botaosom3].pick_random()  # Escolhe aleatoriamente um dos sons
-	sound_player.play()
-	await sound_player.finished
-	
-	match _button.name:
-		"sair_button": 
-			emit_signal("change_scene_to", "Menu")
+#func _on_button_pressed(_button : Button) -> void:
+	##var sound_player = [$botaosom, $botaosom2, $botaosom3].pick_random()  # Escolhe aleatoriamente um dos sons
+	##sound_player.play()
+	##await sound_player.finished
+	#
+	#match _button.name:
+		#"sair_button": 
+			#self.get_parent().close_option()
 
 
 func _on_controle_volume_value_changed(value: float) -> void:
--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -7,20 +7,27 @@ const REGRAS_SCENE := preload("res://scenes/Menu/Regras/regras.tscn")
 const CONFIG_SCENE := preload("res://scenes/Menu/Configuracoes/menu_configuracao.tscn")
 const CREDITOS_SCENE := preload("res://scenes/Menu/Creditos/creditos.tscn")
 
-
-@onready var buttons_box: MarginContainer = $ButtonsBox
 var current_option: Node = null
 
 
 func open_option(option: String) -> void:
-	buttons_box.visible = true
+	$ButtonsBox.visible = false
+	$titulo.visible = false
 	match option:
 		"Regras":
-			pass
+			current_option = REGRAS_SCENE.instantiate()
+			self.add_child(current_option)
 		"Config":
-			pass
+			current_option = CONFIG_SCENE.instantiate()
+			self.add_child(current_option)
 		"Creditos":
-			pass
+			current_option = CREDITOS_SCENE.instantiate()
+			self.add_child(current_option)
+
+func close_option() -> void:
+	current_option.queue_free()
+	$ButtonsBox.visible = true
+	$titulo.visible = true
 
 func _ready() -> void:
 	get_parent().connect_change_scene_signals(self)
@@ -30,15 +37,15 @@ func _ready() -> void:
 
 
 func _on_button_pressed(_button : Button) -> void:
-	var audio = get_node("somfundo")
-	# Garantir que o áudio esteja na árvore de nós principal
-	if audio.get_parent() != get_tree().root:
-		audio.reparent(get_tree().root)  # Move para a raiz da árvore
-		audio.owner = null  # Garante que o nó não seja deletado
+	#var audio = get_node("somfundo")
+	## Garantir que o áudio esteja na árvore de nós principal
+	#if audio.get_parent() != get_tree().root:
+		#audio.reparent(get_tree().root)  # Move para a raiz da árvore
+		#audio.owner = null  # Garante que o nó não seja deletado
   
-	var sound_player = [$botaosom, $botaosom2, $botaosom3].pick_random()  	# Escolhe aleatoriamente um dos sons
-	sound_player.play()
-	await sound_player.finished
+	#var sound_player = [$botaosom, $botaosom2, $botaosom3].pick_random()  	# Escolhe aleatoriamente um dos sons
+	#sound_player.play()
+	#await sound_player.finished
 	
 	match _button.name:
 		"jogar_button":
@@ -46,13 +53,10 @@ func _on_button_pressed(_button : Button) -> void:
 		"tutorial_button":
 			emit_signal("change_scene_to", "Tutorial")
 		"regras_button":
-			#emit_signal("change_scene_to", "Regras")
 			open_option("Regras")
 		"configuracao_button":
-			#emit_signal("change_scene_to", "Config")
 			open_option("Config")
 		"creditos_button":
-			emit_signal("change_scene_to", "Creditos")
 			open_option("Creditos")
 		"sair_button":
 			get_tree().quit()
