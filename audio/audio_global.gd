23361f522810d06e9ee84dc02fb3b1d0f6f6a4db
Álvaro Stein <alvarostein@hotmail.com.br>
2025-06-27 13:35:58 -0300

    sfx finished

--- audio/audio_global.gd
+++ audio/audio_global.gd
@@ -6,12 +6,11 @@ extends Node
 @onready var score_spin: AudioStreamPlayer = $ScoreSpin
 @onready var defeat: AudioStreamPlayer = $Defeat
 
+var master_bus_idx := AudioServer.get_bus_index("Master")
+var sfx_bus_idx := AudioServer.get_bus_index("Sfx")
+var music_bus_idx := AudioServer.get_bus_index("Music")
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+func _ready() -> void:
+	AudioServer.set_bus_volume_db(master_bus_idx, linear_to_db(0.5))
+	AudioServer.set_bus_volume_db(sfx_bus_idx, linear_to_db(0.5))
+	AudioServer.set_bus_volume_db(music_bus_idx, linear_to_db(0.5))
