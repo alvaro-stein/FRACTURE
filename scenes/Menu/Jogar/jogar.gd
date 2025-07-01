4a4583c48dcb64332118bb961cce314c38d7709d
Álvaro Stein <alvarostein@hotmail.com.br>
2025-06-30 23:00:18 -0300

    new multiplayer warning

--- scenes/Menu/Jogar/jogar.gd
+++ scenes/Menu/Jogar/jogar.gd
@@ -4,27 +4,6 @@ signal change_scene_to
 
 func _ready() -> void:
 	get_parent().get_parent().connect_change_scene_signals(self)
-	#
-	#for _button in get_tree().get_nodes_in_group("buttons"):
-		#_button.pressed.connect(_on_button_pressed.bind(_button))
-#
-#
-#func _on_button_pressed(_button : Button) -> void:
-	#match _button.name:
-		#"Singleplayer":
-			##emit_signal("change_scene_to", "Match")
-			##open_option("Jogar")
-			#pass
-		#"Multiplayer":
-			#pass
-		#"Tutorial":
-			#emit_signal("change_scene_to", "TutorialDeck")
-		#"Regras":
-			##open_option("Regras")
-			#pass
-		#"Voltar":
-			#self.get_parent().close_option()
-			
 
 
 func _on_singleplayer_pressed() -> void:
@@ -33,9 +12,14 @@ func _on_singleplayer_pressed() -> void:
 	self.get_parent().open_option("SingleplayerSelection")
 
 
+var tween: Tween # Usando só pra label do aviso temporário do multiplayer
 func _on_multiplayer_pressed() -> void:
 	AudioGlobal.button.play()
-	pass # Replace with function body.
+	if tween:
+		tween.stop()
+	tween = self.create_tween()
+	$ButtonsBox/Multiplayer/Label.modulate.a = 1
+	tween.tween_property($ButtonsBox/Multiplayer/Label, "modulate:a", 0, 2)
 
 
 func _on_tutorial_pressed() -> void:
